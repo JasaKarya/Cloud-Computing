2954ad2b1e90327a3de41e179ef44632dca6b181
Nopi <126689111+novyanywr@users.noreply.github.com>
2023-12-22 12:12:27 +0700

    Delete login-register flask directory

--- login-register flask/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Get the base directory of the current file
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Define the SQLite database URI
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "db.sqlite")
